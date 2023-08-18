9ad0a6b8f5c035c4390610538d3f2b71ee2b1021
Tiago Rafael <tiago.rafael019@gmail.com>
2023-08-18 16:42:12 -0300

    Carregando arquivo de coordenadas a partir de um JSON

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Menu from './components/menu/Menu';
 import Produtos from './components/Produtos';
 import Maps from './components/Maps';
 import React from 'react';
+import heatMapData from './date/heatMapData.json'
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App"> 
       <Menu/>
-      <Maps/>
+      <Maps heatMapData={heatMapData}/>
     </div>
   );
 }
