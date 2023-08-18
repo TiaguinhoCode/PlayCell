58b773d04185f7c4a13eb27f811644c9cc3af495
Tiago Rafael <tiago.rafael019@gmail.com>
2023-08-18 13:23:59 -0300

    Mapa de calor adicionando

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,29 +1,81 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
+import GoogleMapReact from 'google-map-react'; 
 
 function Maps() {
+
+    // Chave da API
+    const MapWithHeatmap = () => {
+        const bootstrapURLKeys = {
+            key: "AIzaSyA0AzG2QAULAT-nU6-Tem2xyxYofoodNi4",
+        };
+
+        // Map calor
+        const heatMapData = [
+            { lat: 37.775, lng: -122.434 },
+            { lat: 37.775, lng: -122.434 },
+            { lat: 37.782, lng: -122.445 },
+        ];
+
+        // Variavel de zoom e centralizar o map
+        const center = {lat: 37.775, lng: -122.434};
+        const zoom = 10;
+
+        return (
+            <div className="map" >
+                <GoogleMapReact
+                    bootstrapURLKeys={bootstrapURLKeys}
+                    defaultCenter={center}
+                    defaultZoom={zoom}
+                    heatmap={{
+                        positions: heatMapData,
+                        options: { radius: 20 }
+                    }}
+                >
+                </GoogleMapReact>
+            </div>
+        );
+    }
+
+
+    return (
+        <div className="map">
+            <MapWithHeatmap />
+        </div>
+    )
+}
+
+export default Maps;
+
+/* Antigo Código
 
     const { isLoaded } = useJsApiLoader({
         id: "google-map-script",
         GoogleMapApiKey: "AIzaSyA0AzG2QAULAT-nU6-Tem2xyxYofoodNi4"
     });
-
-
-    return (
-        <div className="map">
-            isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={ { width: "100%", height: "100%"} }
-        center={ {
-            lat: -3.728464,
-            lng: -38.529904
-        }}
-        zoom={15}
-      ></GoogleMap>
-  ) 
-
+ return (
+        <div className="map">  
+        
+            {isLoaded && (
+                <GoogleMap
+                    mapContainerStyle={{ width: "100%", height: "100%" }}
+                    center={{ lat: -3.728464, lng: -38.529904 }}
+                    zoom={15}
+                />
+            )}
         </div>
     );
-}
+*/
 
-export default Maps
+/* Backup GoogleMap 
+    <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyA0AzG2QAULAT-nU6-Tem2xyxYofoodNi4" }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                <AnyReactComponent
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="Meu marcador"
+                />
+            </GoogleMapReact>
+*/
